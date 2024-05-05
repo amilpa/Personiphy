e5b1737dd7b1df185694d25ba1a06b726cce7749
amilpa <amilpa2020@gmail.com>
2024-05-05 20:00:40 +0530

    fixed ML output

--- src/app/api/createOutput/route.js
+++ src/app/api/createOutput/route.js
@@ -1,42 +1,66 @@
-import writeXlsxFile from "write-excel-file"
-import { NextResponse } from "next/server"
-import axios from "axios"
-import path from "path"
-import fs from "fs"
+import axios from "axios";
+import { NextResponse } from "next/server";
+import { utils, write } from "xlsx";
 
-export async function POST(request){
-    try { 
-        const result=await request.json()
-        const filePath=path.join(process.cwd(),"Trial.xlsx")    
-    
-        let obj_h=[]
-        let obj_d=[]
-    
-        for(let i=0;i<result.length;i++){
-            obj_h=[...obj_h,result[i].Qtype]
-            obj_d=[...obj_d,result[i].Ans]
-        }
-    
-        const data=[obj_h,obj_d]
-        await writeXlsxFile(data,{filePath:filePath})
+export async function POST(request) {
+  try {
+    const result = await request.json();
 
-        // const blob = new Blob([fileData], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+    let worksheet = utils.json_to_sheet(result);
+    let workbook = utils.book_new();
+    utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    let binaryString = write(workbook, { type: "binary" });
+    let formData = new FormData();
 
-        // // Create FormData
-        // const formData = new FormData();
-        // formData.append("file", blob, "Trial.xlsx");
+    formData.append(
+      "file",
+      new Blob([
+        new Uint8Array([...binaryString].map((char) => char.charCodeAt(0))),
+      ]),
+      "file.xlsx"
+    );
 
-        // const response=await axios.post("https://personiphy.onrender.com/predict",formData, {
-        //     headers: {
-        //         "Content-Type": "multipart/form-data"
-        //     }
-        // })
-        
-        // console.log(response)
-        return NextResponse.json({msg:"Created file",filePath:filePath})
-        
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({msg:error})
-    }
+    console.log("starting");
+    const res = await axios.post(
+      "https://personiphy.onrender.com/predict",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          accept: "application/json",
+        },
+      }
+    );
+    console.log(res.data);
+
+    // let obj_h=[]
+    // let obj_d=[]
+
+    // for(let i=0;i<result.length;i++){
+    //     obj_h=[...obj_h,result[i].Qtype]
+    //     obj_d=[...obj_d,result[i].Ans]
+    // }
+
+    // const data=[obj_h,obj_d]
+    // await writeXlsxFile(data,{filePath:filePath})
+
+    // const blob = new Blob([fileData], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+
+    // // Create FormData
+    // const formData = new FormData();
+    // formData.append("file", blob, "Trial.xlsx");
+
+    // const response=await axios.post("https://personiphy.onrender.com/predict",formData, {
+    //     headers: {
+    //         "Content-Type": "multipart/form-data"
+    //     }
+    // })
+
+    // console.log(response)
+    return NextResponse.json(res.data);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ msg: "Error occured" });
+    return NextResponse.json({ msg: error });
+  }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,53 @@
-"use client"
-import {Kaushan_Script} from "next/font/google";
-import ButtonCard from "./ButtonCard";
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Kaushan_Script } from "next/font/google";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
-const ks=Kaushan_Script({subsets:["latin"],weight:"400"})
+import ButtonCard from "./ButtonCard";
+const ks = Kaushan_Script({ subsets: ["latin"], weight: "400" });
 
 export default function Navbar() {
-  const {data:session}=useSession({})
+  const { data: session } = useSession({});
 
   return (
-    <div className="flex justify-between bg-white fixed w-full pb-4">{/* this is the Header/Navbar */}
-        <div className={`mt-9 ml-10 text-3xl ${ks.className}`}>
-          <Link href="/">
-            Personiphy
-          </Link>
-        </div>
+    <div className="flex justify-between bg-white fixed w-full pb-4">
+      {/* this is the Header/Navbar */}
+      <div className={`mt-9 ml-10 text-3xl ${ks.className}`}>
+        <Link href="/">Personiphy</Link>
+      </div>
 
-        <div className="flex mt-10 ml-20">
-          <div className="mx-8 text-xl">About Us</div>
-          <div className="mx-8 text-xl" >Teams</div>
-          <div className="mx-8 text-xl">Contact</div>
-        </div>
+      <div className="flex mt-10 ml-20">
+        <div className="mx-8 text-xl">About Us</div>
+        <div className="mx-8 text-xl">Teams</div>
+        <div className="mx-8 text-xl">Contact</div>
+      </div>
 
-        <div className="flex mt-8">
-          {
-          session ?
-            <>
-              <div className="mx-8 text-center py-3 font-bold text-lg">{session.user.name}</div>
-              <Link href="/api/auth/signout"><ButtonCard content="Logout" color="#0DD299" w="6.5"/></Link>
-            </>
-            :
-            <>
-              {/* <div className="mx-8 text-center py-3 font-bold text-lg">Login</div> */}
-              <Link href="/api/auth/signin"><ButtonCard content="Login" color="#0DD299" w="6.5"/></Link> 
-              {/* <Link href="/api/auth/register"><ButtonCard content="Register" color="#0DD299" w="6.5"/></Link> */}
-            </>
-          }
-      
-        </div>
-
+      <div className="flex mt-8">
+        {session ? (
+          <>
+            <div className="mx-8 text-center py-3 font-bold text-lg">
+              {session.user.name}
+            </div>
+            <ButtonCard
+              onClick={() => signOut()}
+              content="Logout"
+              color="#0DD299"
+              w="6.5"
+            />
+          </>
+        ) : (
+          <>
+            {/* <Link href="/api/auth/signin">
+              <ButtonCard content="Login" color="#0DD299" w="6.5" />
+            </Link> */}
+            <ButtonCard
+              onClick={() => signIn("google")}
+              content="Login"
+              color="#0DD299"
+              w="6.5"
+            />
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 }
-
